cc2e3c41416ee2c0307fc53a71c97e23626037da
adityaslab <201394@juitsolan.in>
2022-04-19 14:01:22 +0530

    unicafe2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + bad + neutral
 
   const incgood = () => {
     setGood(good+1)
@@ -27,6 +28,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {good * 100 / total} %</p>
     </div>
   )
 }
